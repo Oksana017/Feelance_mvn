cd7d04d5798fba5434f0b3156551c71df874767f
Oksana <oksana017m@mail.com>
2023-08-05 12:57:31 +0300

    add src

---
+++ src/test/java/ru/netology/services/HolidayServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HolidayServiceTest {
+
+    @Test
+    public void holidayIfThresholdMoreThan20000() {
+        HolidayService service = new HolidayService();
+
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void holidayIfThresholdMoreThan150000() {
+        HolidayService service = new HolidayService();
+
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
